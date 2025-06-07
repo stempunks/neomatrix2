de00b1d7831ff15bc9f546d2a73bf6bb354bc5c4
stempunks <michael@stempunks.com.au>
2025-06-07 11:01:11 +1000

    2

--- ines-led-matrix.ts
+++ ines-led-matrix.ts
@@ -17,7 +17,7 @@
 
 // TODO use matrixWidth and matrixHeight and figure out how to implement images and handel matrix-luts with these variables size
 
-//% color=#3162a3 icon="\uf00a" block="NeoMatrix"
+//% color=#000000 icon="\uf00a" block="NeoMatrix"
 namespace lumaMatrix {
     
     /* GLOBAL VARIABLES */
